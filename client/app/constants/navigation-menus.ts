208fe31a5ea4779abded03810440be8d3b581f96
noor-selise <noor.mohammad@selisegroup.com>
2026-04-29 18:29:55 +0600

    Refactor navigation menus and update OIDC redirect path
    - Commented out the Overview menu item and its separator in the navigation menus for future consideration.
    - Changed the OIDC redirect path from '/console' to '/services/language' to align with updated routing requirements.

--- client/app/constants/navigation-menus.ts
+++ client/app/constants/navigation-menus.ts
@@ -1,18 +1,18 @@
 import { Menu } from "@/models/menu-models";
-import { BookMinus, Home, Languages, Package, Settings, Users } from "lucide-react";
+import { BookMinus, Languages, Package, Settings, Users } from "lucide-react";
 
 export const navigationMenus: Menu[] = [
-  {
-    id: "overview-project",
-    type: "menu",
-    name: "Overview",
-    path: "/dashboard",
-    icon: Home,
-  },
-  {
-    type: "separator",
-    id: "separator-overview",
-  },
+  // {
+  //   id: "overview-project",
+  //   type: "menu",
+  //   name: "Overview",
+  //   path: "/dashboard",
+  //   icon: Home,
+  // },
+  // {
+  //   type: "separator",
+  //   id: "separator-overview",
+  // },
   {
     id: "environments",
     type: "menu",
