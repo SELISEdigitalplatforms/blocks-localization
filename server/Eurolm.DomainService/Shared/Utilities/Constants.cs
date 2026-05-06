742d9f31198f6462a0b373c74812a7ea322fe6cc
Md. Rifath Sahriar <Anubis1080p@gmail.com>
2026-05-06 13:04:44 +0600

    fix: update queue constants to match new naming conventions in Constants.cs and ConstantsTests.cs

--- server/Eurolm.DomainService/Shared/Utilities/Constants.cs
+++ server/Eurolm.DomainService/Shared/Utilities/Constants.cs
@@ -4,12 +4,11 @@ namespace Eurolm.DomainService.Utilities
 {
     public static class Constants
     {
-        public const string UilmQueue = "blocks_uilm_listener";
-        public const string UilmImportExportQueue = "blocks_uilm_import_export_listener";
-        public const string TranslateAllKeysQueue = "blocks_uilm_translate_all_keys_listener";
-        public const string TranslateBlocksLanguageKeyQueue = "blocks_uilm_translate_blocks_language_key_listener";
-        public const string EnvironmentDataMigrationQueue = "blocks_uilm_environment_data_migration_listener";
-
+        public const string UilmQueue = "eurolm_listener";
+        public const string UilmImportExportQueue = "eurolm_import_export_listener";
+        public const string TranslateAllKeysQueue = "eurolm_translate_all_keys_listener";
+        public const string TranslateBlocksLanguageKeyQueue = "eurolm_translate_blocks_language_key_listener";
+        public const string EnvironmentDataMigrationQueue = "eurolm_environment_data_migration_listener";
         private const string DefaultProvider = "azure";
         private const string RabbitMqProvider = "rabbitmq";
 
