202afcfab5b3bc6e445d2ce3058eed7949871274
Md. Rifath Sahriar <Anubis1080p@gmail.com>
2026-05-19 00:37:25 +0600

    feat: implement fallback for index.html with async response handling

--- server/Api/Program.cs
+++ server/Api/Program.cs
@@ -47,10 +47,42 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 
 var indexHtml = Path.Combine(app.Environment.WebRootPath ?? "", "index.html");
+// if (File.Exists(indexHtml))
+// {
+//     app.MapFallbackToFile("/index.html");
+//     // x-blocks-key cookie
+// }
 if (File.Exists(indexHtml))
 {
-    app.MapFallbackToFile("/index.html");
+
+    app.MapFallback(async context =>
+    {
+        // var tenantService = context.RequestServices.GetRequiredService<ITenants>();
+        // var dbContext = context.RequestServices.GetRequiredService<IDbContextProvider>();
+        // var host = context.Request.Host.Value;
+        // var tenant = tenantService.GetTenantByApplicationDomain(host);
+        // var database = dbContext.GetDatabase(tenant.TenantId);
+        // var captcheSetting = await (await database.GetCollection<CaptchaConfiguration>("CaptchaConfigurations").FindAsync(Builders<CaptchaConfiguration>.Filter.Eq(mc => mc.IsEnable, true))).FirstOrDefaultAsync();
+        // ApplyFrontendRuntimeSettings(builder.Configuration, wwwrootPath, tenant.TenantId, captcheSetting.CaptchaKey);
+
+        //context.Response.Cookies.Append("x-blocks-key", tenant.TenantId, new CookieOptions
+        //{
+        //    Domain = tenant.CookieDomain,
+        //    HttpOnly = true,
+        //    Secure = true,
+        //    SameSite = SameSiteMode.None,
+        //    Path = "/"
+        //});
+        context.Response.ContentType = "text/html; charset=utf-8";
+        await context.Response.SendFileAsync(indexHtml);
+
+    });
+
     // x-blocks-key cookie
+    // check if domain match 
+    // get google captch key BLOCKS_GOOGLE_SITE_KEY
+    // Base Url 
+    // Construct URL 
 
 }
 
