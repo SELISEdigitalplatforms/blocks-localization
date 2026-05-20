8573ef2a3a15ad6442e0a51a2aa9694dd6b6e4fb
noor-selise <noor.mohammad@selisegroup.com>
2026-05-20 18:17:14 +0600

    refactor: improve formatting and consistency in Logo component

--- client/app/components/logo/index.tsx
+++ client/app/components/logo/index.tsx
@@ -1,34 +1,40 @@
 interface LogoProps {
-  src?: string;
-  alt?: string;
-  width?: number;
-  height?: number;
-  className?: string;
+  src?: string
+  alt?: string
+  width?: number
+  height?: number
+  className?: string
 }
 
-
 export function Logo({ src, alt, width, height, className }: LogoProps) {
-
   if (src) {
-    return <img src={src} alt={alt ?? "SELISE Logo"} width={width} height={height} className={className} />;
+    return (
+      <img
+        src={src}
+        alt={alt ?? 'SELISE Logo'}
+        width={width}
+        height={height}
+        className={className}
+      />
+    )
   }
 
   return (
     <>
       <img
-        src="/Logo_Black.svg"
-        alt={alt ?? "SELISE Logo"}
+        src='/Logo_Black.svg'
+        alt={alt ?? 'SELISE Logo'}
         width={width}
         height={height}
-        className={`${className ?? ""} dark:hidden`}
+        className={`${className ?? ''} dark:hidden`}
       />
       <img
-        src="/Logo_White.svg"
-        alt={alt ?? "SELISE Logo"}
+        src='/Logo_White.svg'
+        alt={alt ?? 'SELISE Logo'}
         width={width}
         height={height}
-        className={`${className ?? ""} hidden dark:block`}
+        className={`${className ?? ''} hidden dark:block`}
       />
     </>
-  );
+  )
 }
