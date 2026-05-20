812a1b72809e20c21b94efcbbc0f4f0e75b66823
noor-selise <noor.mohammad@selisegroup.com>
2026-05-20 20:19:57 +0600

    feat: enhance impersonation functionality with initialization state and refactor guards

--- client/app/layouts/dashboard-layout.tsx
+++ client/app/layouts/dashboard-layout.tsx
@@ -1,24 +1,24 @@
-import { Outlet } from "react-router-dom";
 import { DashboardLayoutProvider } from "@/contexts/dashboard-layout-provider";
-import { ImpersonateGuard, ProtectedGuard } from "@/guards/protected-guard";
-import { SidebarMenuDesktop } from "@/layouts/sidebar-menu-desktop/sidebar-menu-desktop";
+import { ImpersonationChecker, ProtectedGuard } from "@/guards/protected-guard";
 import { DashboardHeader } from "@/layouts/dashboard-header/dashboard-header";
+import { SidebarMenuDesktop } from "@/layouts/sidebar-menu-desktop/sidebar-menu-desktop";
+import { Outlet } from "react-router-dom";
 export function DashboardLayout() {
   return (
     <ProtectedGuard>
-      <ImpersonateGuard>
+      <ImpersonationChecker>
         <DashboardLayoutProvider isOpen={true} persist>
-          <div className="relative flex h-screen overflow-hidden bg-[hsl(var(--surface-app))]">
+          <div className='relative flex h-screen overflow-hidden bg-[hsl(var(--surface-app))]'>
             <SidebarMenuDesktop />
-            <div className="flex min-w-0 flex-1 flex-col overflow-hidden">
+            <div className='flex min-w-0 flex-1 flex-col overflow-hidden'>
               <DashboardHeader />
-              <main className="flex-1 overflow-y-auto overflow-x-hidden">
+              <main className='flex-1 overflow-y-auto overflow-x-hidden'>
                 <Outlet />
               </main>
             </div>
           </div>
         </DashboardLayoutProvider>
-      </ImpersonateGuard>
+      </ImpersonationChecker>
     </ProtectedGuard>
-  );
+  )
 }
