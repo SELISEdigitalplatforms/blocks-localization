773498b140ea4fc7bf01759013cb8dfba76b25b3
noor-selise <noor.mohammad@selisegroup.com>
2026-06-07 18:11:19 +0600

    Refactor code structure for improved readability and maintainability

--- client/app/main.tsx
+++ client/app/main.tsx
@@ -5,15 +5,15 @@ import { NuqsAdapter } from "nuqs/adapters/react-router/v6";
 import { Toaster } from "./components/ui-kits/toaster/toaster";
 import QueryProvider from "./providers/query-provider";
 import { router } from "./router";
-import { ThemeProvider } from "./hooks/use-theme";
 import { BlocksAppLayout } from '@seliseblocks/blocks-kit'
+import { TooltipProvider } from "./components/ui-kits/tooltip/tooltip";
 import "./styles/globals.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryProvider>
-      <ThemeProvider>
-        <NuqsAdapter>
+      <NuqsAdapter>
+        <TooltipProvider>
           <BlocksAppLayout
             config={{
               userBaseUrlKey: 'BLOCKS_IAM_BASE_URL',
@@ -23,8 +23,8 @@ createRoot(document.getElementById('root')!).render(
             <RouterProvider router={router} />
           </BlocksAppLayout>
           <Toaster />
-        </NuqsAdapter>
-      </ThemeProvider>
+        </TooltipProvider>
+      </NuqsAdapter>
     </QueryProvider>
   </StrictMode>,
 )
