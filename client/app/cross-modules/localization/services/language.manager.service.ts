c67e68412da5026b7de21d0f8912d382cd6b1463
Sangay Thinley <59992112+sangayt1997@users.noreply.github.com>
2026-06-30 18:23:04 +0600

    fix(blocks-localization): fixed the delete key issues

--- client/app/cross-modules/localization/services/language.manager.service.ts
+++ client/app/cross-modules/localization/services/language.manager.service.ts
@@ -176,18 +176,19 @@ export class LanguageManagerService {
     return this.httpClient.post(url, payload);
   };
 
-  deleteLanguageKey(payload: { itemId: string; projectKey: string }): Promise<{
+  deleteLanguageKey = (payload: {
+    itemId: string;
+    ProjectKey: string;
+  }): Promise<{
     errors: null | unknown;
     isSuccess: boolean;
-  }> {
+  }> => {
     const url = LANGUAGE_KEY_ENDPOINTS.DELETE;
-    const fullUrl = `${url}?itemId=${payload.itemId}&projectKey=${payload.projectKey}`;
-    console.log("[DEBUG service] deleteLanguageKey request URL:", fullUrl);
     return this.httpClient.delete<{
       errors: unknown;
       isSuccess: boolean;
-    }>(fullUrl);
-  }
+    }>(`${url}?itemId=${payload.itemId}&ProjectKey=${payload.ProjectKey}`);
+  };
 
   deleteLanguageKeys(payload: {
     itemIds: string[];
