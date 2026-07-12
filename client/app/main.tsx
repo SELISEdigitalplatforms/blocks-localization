e3d0f6c8f0565afe07d5b148e263d9e094bbd8d4
Sangay Thinley <59992112+sangayt1997@users.noreply.github.com>
2026-07-12 12:44:59 +0600

    refactor(blocks-localization): updating the blocks-kit to v0.0.59

--- client/app/main.tsx
+++ client/app/main.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "./components/ui-kits/toaster/toaster";
 import QueryProvider from "./providers/query-provider";
 import { router } from "./router";
 import { BlocksAppLayout } from "@seliseblocks/blocks-kit";
-import { ThemeProvider } from "@/hooks/use-theme";
+import { ThemeProvider } from "@seliseblocks/blocks-kit/providers";
 import { TooltipProvider } from "./components/ui-kits/tooltip/tooltip";
 import "./styles/globals.css";
 
